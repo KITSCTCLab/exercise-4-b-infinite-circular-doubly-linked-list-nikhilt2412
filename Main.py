75cf014fd0fc52d3ec53136e3d22eb06c9fb9543
nikhilt2412 <110010400+nikhilt2412@users.noreply.github.com>
2022-11-05 13:23:16 +0530

    4.5

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ length_of_circular_linked_list = int(input())
 circular_linked_list = list(map(int,input().strip().split(" ")))
 actual_list = []
 index = 0
+
 while len(actual_list) < length_of_circular_linked_list and index < len(circular_linked_list):
     element = circular_linked_list[index]
     if element not in actual_list:
